c0212de2acef5bc88c6611a2ab488d571371c63c
Paweł <pawelzet00.pz@gmail.com>
2021-04-25 16:01:50 +0200

    Search: - views - home - prestage - no go, - navbar - patch go, - templates - no go.

--- Post_API/views.py
+++ Post_API/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, HttpResponse
 from django.contrib.auth.decorators import login_required
+from Login_API.models import UserProfile
+from django.contrib.auth.models import User
 
 
 @login_required
 def home(request):
-    return render(request, 'Post_API/home.html', context = {'title' : 'Home Page'})
+    if request.method == 'GET':
+        search = request.GET.get('search', '')
+        result = User.objects.filter(username__icontains = search)
+    return render(request, 'Post_API/home.html', context = {'title' : 'Home Page', 'search' : search, 'result' : result})
